ef04e529cecb1fdd18689140f2f93dd3501e6548
Jerome Terry <jerome.l.terry@gmail.com>
2026-05-04 20:02:32 -0230

    Updating to v4.7.1 of IR module

--- IR_Receiver_Module/IR_Receiver_Module.ino
+++ IR_Receiver_Module/IR_Receiver_Module.ino
@@ -1,75 +1,33 @@
-//www.elegoo.com
-//2023.05.05
+#include <IRremote.hpp>
 
-#include "IRremote.h"
+const int IR_RECEIVE_PIN = 9;
 
-int receiver = 11; // Signal Pin of IR receiver to Arduino Digital Pin 11
-
-/*-----( Declare objects )-----*/
-IRrecv irrecv(receiver);     // create instance of 'irrecv'
-//vairable uses to store the last decodedRawData
-uint32_t last_decodedRawData = 0;
-/*-----( Function )-----*/
-void translateIR() // takes action based on IR code received
-{
-  // Check if it is a repeat IR code 
-  if (irrecv.decodedIRData.flags)
-  {
-    //set the current decodedRawData to the last decodedRawData 
-    irrecv.decodedIRData.decodedRawData = last_decodedRawData;
-    Serial.println("REPEAT!");
-  } else
-  {
-    //output the IR code on the serial monitor
-    Serial.print("IR code:0x");
-    Serial.println(irrecv.decodedIRData.decodedRawData, HEX);
-  }
-  //map the IR code to the remote key
-  switch (irrecv.decodedIRData.decodedRawData)
-  {
-    case 0xBA45FF00: Serial.println("POWER"); break;
-    case 0xB847FF00: Serial.println("FUNC/STOP"); break;
-    case 0xB946FF00: Serial.println("VOL+"); break;
-    case 0xBB44FF00: Serial.println("FAST BACK");    break;
-    case 0xBF40FF00: Serial.println("PAUSE");    break;
-    case 0xBC43FF00: Serial.println("FAST FORWARD");   break;
-    case 0xF807FF00: Serial.println("DOWN");    break;
-    case 0xEA15FF00: Serial.println("VOL-");    break;
-    case 0xF609FF00: Serial.println("UP");    break;
-    case 0xE619FF00: Serial.println("EQ");    break;
-    case 0xF20DFF00: Serial.println("ST/REPT");    break;
-    case 0xE916FF00: Serial.println("0");    break;
-    case 0xF30CFF00: Serial.println("1");    break;
-    case 0xE718FF00: Serial.println("2");    break;
-    case 0xA15EFF00: Serial.println("3");    break;
-    case 0xF708FF00: Serial.println("4");    break;
-    case 0xE31CFF00: Serial.println("5");    break;
-    case 0xA55AFF00: Serial.println("6");    break;
-    case 0xBD42FF00: Serial.println("7");    break;
-    case 0xAD52FF00: Serial.println("8");    break;
-    case 0xB54AFF00: Serial.println("9");    break;
-    default:
-      Serial.println(" other button   ");
-  }// End Case
-  //store the last decodedRawData
-  last_decodedRawData = irrecv.decodedIRData.decodedRawData;
-  delay(500); // Do not get immediate repeat
-} //END translateIR
-
-void setup()   /*----( SETUP: RUNS ONCE )----*/
-{
+void setup() {
   Serial.begin(9600);
-  Serial.println("IR Receiver Button Decode");
-  irrecv.enableIRIn(); // Start the receiver
+  
+  // The modern v4.x initialization
+  // ENABLE_LED_FEEDBACK will actually blink the built-in Arduino LED (Pin 13) when it sees a signal
+  IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
+  
+  Serial.println("IR Receiver V4.x Diagnostics Online.");
+  Serial.println("Press a button on the remote...");
+}
 
-}/*--(end setup )---*/
-
-
-void loop()   /*----( LOOP: RUNS CONSTANTLY )----*/
-{
-  if (irrecv.decode()) // have we received an IR signal?
-  {
-    translateIR();
-    irrecv.resume(); // receive the next value
+void loop() {
+  // The modern v4.x decode function
+  if (IrReceiver.decode()) {
+    
+    // Ignore repeat codes for cleaner logging
+    if (!(IrReceiver.decodedIRData.flags & IRDATA_FLAGS_IS_REPEAT)) {
+      
+      Serial.print("Protocol: ");
+      Serial.print(IrReceiver.decodedIRData.protocol);
+      Serial.print(" | Command: 0x");
+      Serial.println(IrReceiver.decodedIRData.command, HEX);
+      
+    }
+    
+    // Prime the receiver for the next signal
+    IrReceiver.resume(); 
   }
-}/* --(end main loop )-- */
+}
